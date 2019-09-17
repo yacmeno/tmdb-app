1e85fd1d7f3683ac77d4b4d006b07a990ec27345
yacmeno <yacine.seffal@gmail.com>
2019-09-17 19:58:54 -0400

    Added prettier and formatted files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="class1">
-      Hello, world!
-    </div>
-  );
-}
+	return <div className="class1">Hello, world!</div>;
+};
 
 export default App;
