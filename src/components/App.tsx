e92d1e5820c73755236db1d1978e59823251b5ba
yacmeno <yacine.seffal@gmail.com>
2019-09-27 11:29:43 -0400

    Added navigation skeleton and styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Navigation from "./Navigation";
 
 const App: React.FC = () => {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Navigation />
+		</>
+	);
 };
 
 export default App;
